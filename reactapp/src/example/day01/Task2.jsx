e1983921afa3403f30628f95e25390ed5b8ca717
ãkant300 <p.o.i.u.0987552@gmail.com>
2025-09-16 14:50:19 +0900

    20250916 example/day01 Task2 css

--- reactapp/src/example/day01/Task2.jsx
+++ reactapp/src/example/day01/Task2.jsx
@@ -4,11 +4,15 @@
     { title: "27인치 모니터", price: 280000, inStock: true }
   ]; 
 
+  // [3] CSS파일 불러오기 : import 'CSS파일경로'
+  import './Task2.css'
+   
+
   // [1] 해당 .jsx 파일내 대표(default) 컴포넌트 만들기
   export default function Task2( props ){
     // InfoCard함수의 매개변수로 아무거나속성명 사용
     return(<>
-        <div>
+        <div class="products">
             { /* 하위 컴포넌트 호출과 동시에 props 속성 자료 전달 */}
             <InfoCard product = { products[0] } />
             <InfoCard product = { products[1] } />
@@ -24,9 +28,9 @@
     const { title, price, inStock } = props.product
     return (<>
             <ul>
-                <li> { title }</li>
-                <li> { price.toLocaleString() }</li>
-                <li> { inStock == true ? '재고있음' : '재고없음' } </li>
+                <li><h3> { title } </h3> </li>
+                <li><span class="price"> { price.toLocaleString() } </span> </li>
+                <li> <span class="inStock"> { inStock == true ? '재고있음' : '재고없음' } </span> </li>
             </ul>
         </>)
   }
