27506205f54ccf99881203467f5e35f3d7f76b16
kant300 <p.o.i.u.0987552@gmail.com>
2025-09-24 18:08:51 +0900

    20250924 리덕스+라우터+퍼시스턴스

--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -115,10 +115,15 @@ import Task6 from './example/day04/Task6.jsx';
 // day05 실습7
 import App from './example/day05/실습7/App.jsx';
 import { Provider } from "react-redux";
-import store from './example/day05/실습7/store/store.jsx';
+import store, { persistor } from './example/day05/실습7/store/store.jsx';
+import { PersistGate } from 'redux-persist/integration/react';
 create.render( 
-  // 내가 만든 store 를 root 컴포넌트에 공급하여 모든 컴포넌트가 사용할수 있도록 *전역변수*
+  // [1] 내가 만든 store 를 root 컴포넌트에 공급하여 모든 컴포넌트가 사용할수 있도록 *전역변수*
+  // [2] 내가 만든 persist 공급, loading : { 초기로딩값 } persist = { 내가 만든 persiststore }
   <Provider store={ store } >
     <App />
+    <PersistGate loading = { null } persistor={ persistor }>
+      <App />
+    </PersistGate>
   </Provider>
 );
