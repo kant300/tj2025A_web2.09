6ab03cc90250fbdf54ffeb16740f3f3bc1bc2579
kant300 <p.o.i.u.0987552@gmail.com>
2025-10-13 12:33:48 +0900

    20251013 day91 xml이용한 저장

--- src/main/java/example/day13/XmlController.java
+++ src/main/java/example/day13/XmlController.java
@@ -1,13 +1,12 @@
 package example.day13;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.graphql.ConditionalOnGraphQlSchema;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/xml")
@@ -19,14 +18,41 @@ public class XmlController {
     @PostMapping("")
     public ResponseEntity< ? > save(@RequestBody StudentDto dto){
         // < ? > : 제네릭타입에 ? 넣으면 모든 타입을 지칭한다. 와일드카드
+        System.out.println("dto = " + dto); // soutp , save 실행전 , sno 없다
+        int result = xmlMapper.save(dto);   // ======= SQL 실행 =========
+        System.out.println("dto = " + dto); // soutp , save 실행후 , sno 있다.
         xmlMapper.save( dto );
         return ResponseEntity.ok(true); // 제네릭 ? 이므로 모든 자료가 대입된다.
     }
 
     // # 2. 전체조회
+    @GetMapping("")
     public ResponseEntity< ? > findAll(){
         List<StudentDto> result = xmlMapper.findAll();
         return ResponseEntity.ok( result);
     }
+
+    // # 3. 개별학생조회
+    @GetMapping("/find")    // http://localhost:8080/xml/find?sno=1
+    public ResponseEntity< ? > find( @RequestParam int sno){
+        StudentDto result = xmlMapper.find(sno);
+        return ResponseEntity.ok(result);
+    }
+
+    // # 4. 개별 학생 삭제
+    @DeleteMapping("")  // http://localhost:8080/xml?sno=1
+    public ResponseEntity< ? > delete( @RequestParam int sno ){
+        int result = xmlMapper.delete( sno );
+        return ResponseEntity.ok(result);
+    }
+
+    // # 5. 개별 학생 수정
+    @PutMapping("") // http://localhost:8080/xml
+    // body : { "sno" : "2" , "kor" : "100" , "math" : "100" }
+    public ResponseEntity< ? > update( @RequestBody StudentDto studentDto ){
+        int result = xmlMapper.update( studentDto);
+        return ResponseEntity.ok(result); // ok == 200
+    }
+
 
 }// class e
--- src/main/java/example/day13/XmlMapper.java
+++ src/main/java/example/day13/XmlMapper.java
@@ -1,18 +1,35 @@
 package example.day13;
 
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 
 import java.util.List;
 
-// # 1. 등록
-// Mybatis에서 SQL매핑하는 방법
-// 방법1 : 추상메소드 위에 @Insret("SQL") 작성 , 간단한 SQL 권장
-// 방법2 : 추상메소드를 매핑하는 XML파일에서  SQL 작성,  복잡한 SQL 권장
+
 @Mapper
 public interface XmlMapper {
+    // # 1. 등록
+    // Mybatis에서 SQL매핑하는 방법
+    // 방법1 : 추상메소드 위에 @Insret("SQL") 작성 , 간단한 SQL 권장
+    // 방법2 : 추상메소드를 매핑하는 XML파일에서  SQL 작성,  복잡한 SQL 권장
+
+    // [ 생성된 PK 값 반환하는 방법 ]
+    // @Insert("INSERT INTO student(name, kor, math) VALUES( #{name} , #{kor}, #{math} );")
+    // @Options( useGeneratedKeys = true, keyProperty = "sno") // 생성된 PK를 sno 필드에 매핑
     int save(StudentDto dto);
 
     // # 2. 전체조회
     List<StudentDto> findAll();
+
+    // # 3. 개별 학생 조화
+    StudentDto find (int sno );
+
+    // # 4. 개별 학생 삭제
+    int delete ( int sno );
+
+    // # 5. 개별 학생 수정
+    int update( StudentDto studentDto );
+
 }// i end
